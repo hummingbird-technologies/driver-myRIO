937cb4decc4608cd2e463dfeae90c385e07edee0
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2017-04-16 22:17:49 -0700

    Move variable definitions from device.h to device.c

---
+++ device.c
@@ -0,0 +1,49 @@
+#include "device.h"
+
+struct dio_channel_bank_t
+		a1_dio_channel_bank = {DIOA_70DIR, DIOA_70OUT, DIOA_70IN},
+		a2_dio_channel_bank = {DIOA_158DIR, DIOA_158OUT, DIOA_158IN},
+		b1_dio_channel_bank = {DIOB_70DIR, DIOB_70OUT, DIOB_70IN},
+		b2_dio_channel_bank = {DIOB_158DIR, DIOB_158OUT, DIOB_158IN};
+
+const struct dio_channel_t dio_channels[32] = {
+		// Channel Bank A 0-7
+		{&a1_dio_channel_bank, 0},
+		{&a1_dio_channel_bank, 1},
+		{&a1_dio_channel_bank, 2},
+		{&a1_dio_channel_bank, 3},
+		{&a1_dio_channel_bank, 4},
+		{&a1_dio_channel_bank, 5},
+		{&a1_dio_channel_bank, 6},
+		{&a1_dio_channel_bank, 7},
+
+		// Channel Bank A 8-15
+		{&a2_dio_channel_bank, 0},
+		{&a2_dio_channel_bank, 1},
+		{&a2_dio_channel_bank, 2},
+		{&a2_dio_channel_bank, 3},
+		{&a2_dio_channel_bank, 4},
+		{&a2_dio_channel_bank, 5},
+		{&a2_dio_channel_bank, 6},
+		{&a2_dio_channel_bank, 7},
+
+		// Channel Bank B 0-7
+		{&b1_dio_channel_bank, 0},
+		{&b1_dio_channel_bank, 1},
+		{&b1_dio_channel_bank, 2},
+		{&b1_dio_channel_bank, 3},
+		{&b1_dio_channel_bank, 4},
+		{&b1_dio_channel_bank, 5},
+		{&b1_dio_channel_bank, 6},
+		{&b1_dio_channel_bank, 7},
+
+		// Channel Bank B 8-15
+		{&b2_dio_channel_bank, 0},
+		{&b2_dio_channel_bank, 1},
+		{&b2_dio_channel_bank, 2},
+		{&b2_dio_channel_bank, 3},
+		{&b2_dio_channel_bank, 4},
+		{&b2_dio_channel_bank, 5},
+		{&b2_dio_channel_bank, 6},
+		{&b2_dio_channel_bank, 7},
+};
--- device.h
+++ device.h
@@ -10,11 +10,6 @@ struct dio_channel_bank_t {
     uint32_t in;
 };
 
-struct dio_channel_bank_t
-		a1_dio_channel_bank = {DIOA_70DIR, DIOA_70OUT, DIOA_70IN},
-		a2_dio_channel_bank = {DIOA_158DIR, DIOA_158OUT, DIOA_158IN},
-		b1_dio_channel_bank = {DIOB_70DIR, DIOB_70OUT, DIOB_70IN},
-		b2_dio_channel_bank = {DIOB_158DIR, DIOB_158OUT, DIOB_158IN};
 
 
 struct dio_channel_t {
@@ -22,46 +17,6 @@ struct dio_channel_t {
 	uint8_t bit;
 };
 
-struct dio_channel_t dio_channels[32] = {
-		// Channel Bank A 0-7
-		{&a1_dio_channel_bank, 0},
-		{&a1_dio_channel_bank, 1},
-		{&a1_dio_channel_bank, 2},
-		{&a1_dio_channel_bank, 3},
-		{&a1_dio_channel_bank, 4},
-		{&a1_dio_channel_bank, 5},
-		{&a1_dio_channel_bank, 6},
-		{&a1_dio_channel_bank, 7},
-
-		// Channel Bank A 8-15
-		{&a2_dio_channel_bank, 0},
-		{&a2_dio_channel_bank, 1},
-		{&a2_dio_channel_bank, 2},
-		{&a2_dio_channel_bank, 3},
-		{&a2_dio_channel_bank, 4},
-		{&a2_dio_channel_bank, 5},
-		{&a2_dio_channel_bank, 6},
-		{&a2_dio_channel_bank, 7},
-
-		// Channel Bank B 0-7
-		{&b1_dio_channel_bank, 0},
-		{&b1_dio_channel_bank, 1},
-		{&b1_dio_channel_bank, 2},
-		{&b1_dio_channel_bank, 3},
-		{&b1_dio_channel_bank, 4},
-		{&b1_dio_channel_bank, 5},
-		{&b1_dio_channel_bank, 6},
-		{&b1_dio_channel_bank, 7},
-
-		// Channel Bank B 8-15
-		{&b2_dio_channel_bank, 0},
-		{&b2_dio_channel_bank, 1},
-		{&b2_dio_channel_bank, 2},
-		{&b2_dio_channel_bank, 3},
-		{&b2_dio_channel_bank, 4},
-		{&b2_dio_channel_bank, 5},
-		{&b2_dio_channel_bank, 6},
-		{&b2_dio_channel_bank, 7},
-};
+extern const struct dio_channel_t dio_channels[32];
 
 #endif /* MYRIO_DRIVER_DEVICE_H_ */
